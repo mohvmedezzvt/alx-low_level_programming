0a5feb288be06b7d0a7cb0ce6ee33b8d30d47c93
Mohamed Ezzat <mohamedelhdary321@gmail.com>
2023-03-21 12:35:09 -0700

    a function that prints the n times table, starting with 0.

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -11,33 +11,40 @@ void print_times_table(int n)
 {
 	int i, j, result;
 
-	if (n >= 0 && n <= 15)
+	if (n < 0 || n > 15)
+		return;
+	for (i = 0; i <= n; i++)
 	{
-		for (i = 0; i <= n; i++)
+		for (j = 0; j <= n; j++)
 		{
-			for (j = 0; j <= n; j++)
+			result = i * j;
+			if (j == 0)
+				_putchar(result + '0');
+			else if (result < 10)
 			{
-				result = i * j;
-				if (j == 0)
-					_putchar(result + '0');
-				else if (result < 10)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(result + '0');
-				}
-				else if (result >= 10 && result <= 99)
-				{
-					printf(",  %d", result);
-				}
-				else
-				{
-					printf(", %d", result);
-				}
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(result + '0');
 			}
-			_putchar('\n');
+			else if (result < 100)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(result / 10 + '0');
+				_putchar(result % 10 + '0');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(result / 100 + '0');
+				_putchar((result - 100) / 10 + '0');
+				_putchar(result % 10 + '0');
+			}
 		}
+		_putchar('\n');
 	}
 }
