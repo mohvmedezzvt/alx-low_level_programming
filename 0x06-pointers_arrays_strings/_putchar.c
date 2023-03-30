4e2f3c9be5ddaa124a99015dacf8b32c24c9e30f
Mohamed Ezzat <mohamedelhdary321@gmail.com>
2023-03-30 05:32:27 -0700

    a function that concatenates two strings.

---
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+
+/**
+ * _putchar - writes a single character to the standard output
+ *
+ * @c: the character to write
+ *
+ * Return: On success, returns the character written as an unsigned char.
+ *         On failure, returns -1 and sets errno appropriately.
+ */
+int _putchar(char c)
+{
+	return(write(1, &c, 1));
+}
