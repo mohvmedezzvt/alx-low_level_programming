15ec6edd60625686336f6e814bb3afa60db192d9
Mohamed Ezzat <mohamedelhdary321@gmail.com>
2023-04-05 16:33:24 -0700

    a function that returns 1 if a string is a palindrome and 0 if not.

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * is_palindrome - checks if a string is a palindrome ot not.
- * @s: the string to check.
+ * is_palindrome - checks if a given string is a palindrome.
+ * @s: string to check.
  *
  * Return: 1 if a string is a palindrome and 0 if not.
  */
@@ -12,10 +12,10 @@ int is_palindrome(char *s)
 }
 
 /**
- * is_palindrome_strlen - Gets the string length.
- * @s: the string to git his length.
+ * is_palindrome_strlen - returns the length of a string.
+ * @s: string to check.
  *
- * Return: the string length.
+ * Return: length of the string
  */
 int is_palindrome_strlen(char *s)
 {
@@ -28,20 +28,19 @@ int is_palindrome_strlen(char *s)
 }
 
 /**
- * is_palindrome_checker - checker to determine if the string is a palindrome or not.
- * @s:
- * @first: 
- * @last: 
- * @remainder: 
+ * is_palindrome_helper - checks if a given substring is a palindrom.
+ * @s: string to check.
+ * @left: index of the leftmost character of the substring.
+ * @right: index of the rightmost character of the substring.
  *
- * Return: 1 if a string is a palindrome and 0 if not.
+ * Return: 1 if the substring is a palindrome and 0 if not.
  */
 int is_palindrome_helper(char *s, int left, int right)
 {
 	if (left >= right)
 		return (1);
 	else if (s[left] == s[right])
-		return is_palindrome_helper(s, left + 1, right - 1);
+		return (is_palindrome_helper(s, left + 1, right - 1));
 	else
 		return (0);
 }
