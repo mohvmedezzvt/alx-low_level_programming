5a385210f9b8fe1c2bf39178d8e1c4c23caa8858
Mohamed Ezzat <mohamedelhdary321@gmail.com>
2023-04-08 14:12:30 -0700

    Create the static library libmy.a

---
+++ 0x09-static_libraries/100-atoi.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _atoi - Empty function.
+ * @s: string.
+ *
+ * return: integer
+ */
+int _atoi(char *s)
+{
+	// Empty One.
+}
--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,13 +1,7 @@
 #include "main.h"
 
-/**
- * main - Entry point of the program.
- *
- * Return: 0 always
- */
 int main(void)
 {
-	_puts("\"At the end of the day, \
-			my goal was to be the best hacker\"\n\t- Kevin Mitnick");
+	_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
 	return (0);
 }
