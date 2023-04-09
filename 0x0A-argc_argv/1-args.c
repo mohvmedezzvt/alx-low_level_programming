1a586dc038287b9691b2ca4ec1445cc21d7f4ac8
Mohamed Ezzat <mohamedelhdary321@gmail.com>
2023-04-09 12:59:05 -0700

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 1-args.c -o nargs

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -5,7 +5,7 @@
  *
  * @argc: the number of arguments passed to the program.
  * @argv: an array of strings containing the arguments passed to the program.
- *
+ * 
  * Return: 0 indicating successful program execution.
  */
 int main(int argc, char *argv[])
