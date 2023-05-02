9c9f3b07d588ada6b87b582881d739b0e03b64ce
Mohamed Ezzat <mohamedelhdary321@gmail.com>
2023-05-03 00:55:55 +0200

    a function that inserts a new node at a given position.

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,40 +1,46 @@
 #include "lists.h"
 
+/**
+ * insert_nodeint_at_index - insert a new node at a given position
+ * in a listint_t list.
+ * @head: pointer to the head of the linked list.
+ * @idx: index of the list where the new node should be added.
+ * @n: value to store in the new node.
+ *
+ * Return: the address of the new node, or NULL if it failed.
+*/
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *current, *new;
+	listint_t *current = *head, *new;
 	unsigned int index = 0;
 
-	if (!*head)
+	while (current && index < idx - 1)
+	{
+		current = current->next;
+		index++;
+	}
+
+	if (index != idx - 1)
 		return (NULL);
+
+	if (idx == 0 || !current)
+	{
+		new = malloc(sizeof(listint_t));
+		if (!new)
+			return (NULL);
+
+		new->n = n;
+		new->next = *head;
+		*head = new;
+		return (new);
+	}
 
 	new = malloc(sizeof(listint_t));
 	if (!new)
 		return (NULL);
 
 	new->n = n;
-
-	if (idx == 0)
-	{
-		new->next = *head;
-		*head = new;
-		return (new);
-	}
-
-	current = *head;
-
-	while (index < idx - 1)
-	{
-		if (!current)
-		{
-			free(new);
-			return (NULL);
-		}
-		current = current->next;
-		index++;
-	}
 	new->next = current->next;
 	current->next = new;
-
 	return (new);
 }
