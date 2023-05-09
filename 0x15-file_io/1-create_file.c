d34c9d06718f98cfee9c171b2c7b05d6bb772cbd
Mohamed Ezzat <mohamedelhdary321@gmail.com>
2023-05-09 02:20:31 +0200

    a function that creates a file.

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * create_file - a function that creates a file.
+ * @filename: the name of the file to create.
+ * @text_content: the content to write to the file.
+ *
+ * Return: 1 on success, -1 on failure.
+*/
+int create_file(const char *filename, char *text_content)
+{
+	int fdescriptor, resultWrite, resultClose;
+
+	if (filename == NULL)
+		return (-1);
+
+	fdescriptor = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (fdescriptor == -1)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		resultWrite = write(fdescriptor, text_content, strlen(text_content));
+		if (resultWrite == -1)
+		{
+			close(fdescriptor);
+			return (-1);
+		}
+	}
+
+	resultClose = close(fdescriptor);
+	if (resultClose == -1)
+		return (-1);
+
+	return (-1);
+}
