549bdbaa0952ec9edeafa56f1832f1075e8d4bb2
Mohamed Ezzat <mohamedelhdary321@gmail.com>
2023-06-19 13:40:27 +0200

    Defined a new type struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - Poppy
+ * @name: the dog name.
+ * @age: the dog age.
+ * @owner: the dog owner.
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* _DOG_H_ */
