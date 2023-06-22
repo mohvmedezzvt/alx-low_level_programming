4b5599e7f15bb09f36e44cf6e6091390202c10e8
Mohamed Ezzat <mohamedelhdary321@gmail.com>
2023-06-22 04:22:53 +0200

    a program that performs simple operations.

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,44 @@
+#include "3-calc.h"
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point of the calculator program.
+ * @argc: The number of command-line arguments.
+ * @argv: An array of strings containing the command-line arguments.
+ *
+ * Return: Always 0 (indicating successful execution).
+ */
+int main(int argc, char **argv)
+{
+	int num1, num2, result;
+	int (*f)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+
+	if (num2 == 0 && (strcmp(argv[2], "/") == 0 || strcmp(argv[2], "%") == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+
+	f = get_op_func(argv[2]);
+	if (!f || argv[2][1])
+	{
+		printf("Error");
+		exit(99);
+	}
+
+	result = f(num1, num2);
+	printf("%d\n", result);
+
+	return (0);
+}
