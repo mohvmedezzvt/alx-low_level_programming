d91f4001e14cae2873f1b7701719338d7c5a5b59
Mohamed Ezzat <mohamedelhdary321@gmail.com>
2023-07-01 12:53:32 +0200

    a function that prints before main function is executed

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * before_main - execute before main function
+ *
+ * Return: Nothing.
+*/
+void before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+	"I bore my house upon my back!\n");
+}
