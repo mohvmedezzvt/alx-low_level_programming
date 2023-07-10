f5878ba778ca385b65bcf118b612033ec01f7ae1
Mohamed Ezzat <mohamedelhdary321@gmail.com>
2023-07-10 20:51:52 +0200

    a function that creates a file.

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <string.h>
 
 /**
  * create_file - Creates a file with the specified name and
@@ -10,20 +11,20 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, bytes_writen;
-	mode_t mode = S_IRUSR | S_IWUSR; /* rw------- */
+	int fd;
+	size_t bytes_writen;
 
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, mode);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 		return (-1);
 
 	if (text_content != NULL)
 	{
 		bytes_writen = write(fd, text_content, sizeof(text_content));
-		if (bytes_writen == -1)
+		if (bytes_writen != strlen(text_content))
 		{
 			close(fd);
 			return (-1);
