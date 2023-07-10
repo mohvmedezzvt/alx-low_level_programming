7c57ede42198e6e0fc961a1a2a891c8cce18a8b3
Mohamed Ezzat <mohamedelhdary321@gmail.com>
2023-07-10 20:44:35 +0200

    a function that creates a file.

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,11 +11,12 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd, bytes_writen;
+	mode_t mode = S_IRUSR | S_IWUSR; /* rw------- */
 
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, mode);
 	if (fd == -1)
 		return (-1);
 
