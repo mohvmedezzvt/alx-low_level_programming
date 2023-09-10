99e22ef00cbc1d46c15db60573db4cafc100123e
Mohamed Ezzat <mohamedelhdary321@gmail.com>
2023-09-10 08:57:06 +0300

    Added a function that adds an element to the hash table.

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -15,23 +15,32 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	if (ht == NULL || key == NULL || *key == '\0')
 		return (0);
-
 	index = key_index((const unsigned char *)key, ht->size);
+	current = ht->array[index];
+	while (current != NULL)
+	{
+		if (strcmp(current->key, key) == 0)
+		{
+			free(current->value);
+			current->value = strdup(value);
+			if (current->value == NULL)
+				return (0);
+			return (1);
+		}
+		current = current->next;
+	}
 	new_node = (hash_node_t *)malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
 		return (0);
-
-	new_node = malloc(sizeof(hash_node_t));
+	new_node = (hash_node_t *)malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
 		return (0);
-
 	new_node->key = strdup(key);
 	if (new_node->key == NULL)
 	{
 		free(new_node);
 		return (0);
 	}
-
 	new_node->value = strdup(value);
 	if (new_node->value == NULL)
 	{
@@ -39,14 +48,12 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		free(new_node);
 		return (0);
 	}
-
 	new_node->next = NULL;
 	if (ht->array[index] != NULL)
 	{
 		current = ht->array[index];
 		new_node->next = current;
 	}
-
 	ht->array[index] = new_node;
 	return (1);
 }
