e73984a859fd8d810a6c8d819931965f20dbe79d
Mohamed Ezzat <mohamedelhdary321@gmail.com>
2023-12-12 20:52:56 +0200

    a function that searches for a value in a sorted array of integers using the Jump search algorithm

---
+++ 0x1E-search_algorithms/100-jump.c
@@ -0,0 +1,45 @@
+#include "search_algos.h"
+
+/**
+ * jump_search - searches for a value in a sorted array of integers using the
+ * Jump search algorithm
+ * @array: pointer to the first element of the array to search in
+ * @size: number of elements in the array
+ * @value: value to search for
+ *
+ * Return: first index where value is located, or -1 if value is not present
+ * in array or if array is NULL
+ */
+int jump_search(int *array, size_t size, int value)
+{
+	size_t i, left, right, jump;
+
+	if (array == NULL)
+		return (-1);
+
+	jump = sqrt(size);
+	left = 0;
+	right = jump;
+
+	while (right < size)
+	{
+		printf("Value checked array[%lu] = [%d]\n", left, array[left]);
+		if (array[left] <= value && array[right] >= value)
+			break;
+		left = right;
+		right += jump;
+	}
+
+	printf("Value found between indexes [%lu] and [%lu]\n", left, right);
+	if (right >= size)
+		right = size - 1;
+
+	for (i = left; i <= right; i++)
+	{
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+	}
+
+	return (-1);
+}
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -4,9 +4,11 @@
 /* Libraries */
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 /* Prototypes */
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
 
 #endif /* search_algos_h */
