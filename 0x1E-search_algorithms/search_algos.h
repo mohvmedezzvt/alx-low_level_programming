39985b9b8c166a36d6be9dd384c9da076914c08c
Mohamed Ezzat <mohamedelhdary321@gmail.com>
2023-12-12 19:27:35 +0200

    a function that searches for a value in an array of integers using the Linear search algorithm

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,11 @@
+#ifndef search_algos_h
+#define search_algos_h
+
+/* Libraries */
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Prototypes */
+int linear_search(int *array, size_t size, int value);
+
+#endif /* search_algos_h */
